f6162100a0f291b2a405f4b11ce2658cdac93240
goszpeti <peter.gosztolya@gmail.com>
2024-04-07 16:46:20 +0200

    Update dev_essential, fep_sdk participant and system

--- thirdparty/dev_essential/conanfile.py
+++ thirdparty/dev_essential/conanfile.py
@@ -1,21 +1,34 @@
+from pathlib import Path
 from conans import ConanFile, CMake
+from conan.tools.scm import Git
+from conan.tools.files import copy
+import yaml
+
+required_conan_version = ">=1.59.0"
 
 class ConanProduct(ConanFile):
     name = "dev_essential"
-    version = "1.2.0"
+    version = "1.3.1"
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "txt" 
     options = {"fPIC": [True, False]}
     default_options = {"fPIC": True}
-    scm = {
-        "type": "git",
-        "url": "https://github.com/cariad-tech/dev_essential.git",
-        "revision": "v1.2.0"
-    }
+    no_copy_source = True
+
+    def init(self):
+        if not self.conan_data:
+            # copy single source of truth conan_data from repo root
+            copy(self, "conandata.yml", src=str(Path(__file__).parents[2]), dst=".")
+            self.conan_data = yaml.safe_load(Path("conandata.yml").read_text())
+
+    def source(self):
+       git = Git(self)
+       sources = self.conan_data["sources"][self.name]
+       # shallow checkout
+       git.fetch_commit(url=sources["url"], commit=f"v{self.version}")
 
     def build_requirements(self):
-        if self.settings.arch != "armv8":
-            self.tool_requires("cmake/3.25.0")
+        self.tool_requires(self.conan_data["tool_reqs"]["cmake"])
 
     def build(self):
         cmake = CMake(self)
--- thirdparty/fep3_system/conanfile.py
+++ thirdparty/fep3_system/conanfile.py
@@ -1,45 +1,51 @@
+import os
 from pathlib import Path
-from conans import ConanFile, CMake
-from conan.tools.files import patch
+
+import yaml
+from conan.tools.files import copy, patch
+from conan.tools.scm import Git
+from conans import CMake, ConanFile
+
 
 class ConanProduct(ConanFile):
     name = "fep_sdk_system"
-    version = "3.1.0"
+    version = "3.2.0"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "txt"
+    generators = "cmake", "txt", "CMakeDeps"
     options = {"fPIC": [True, False]}
     default_options = {"fPIC": True, 
-                       "boost:without_date_time":False,
-                       "boost:without_filesystem":False}
+                       "cpython:shared": True,
+                       "boost:without_date_time": False,
+                       "boost:without_filesystem": False}
     no_copy_source = True
     short_paths = True
-    exports_sources = "patch1.diff"
+    exports_sources = "patch.diff"
     no_copy_export_sources = True
-    scm = {
-        "type": "git",
-        "url": "https://github.com/cariad-tech/fep3_system.git",
-        "revision": "v3.1.0"
-    }
-    
-    def configure(self):
-        return super().configure()
+    no_copy_source = True
 
-    def build_requirements(self):
-        if self.settings.arch != "armv8":
-            self.tool_requires("cmake/3.25.0")
+    def init(self):
+        if not self.conan_data:
+            # copy single source of truth conan_data from repo root
+            copy(self, "conandata.yml", src=str(Path(__file__).parents[2]), dst=".")
+            self.conan_data = yaml.safe_load(Path("conandata.yml").read_text())
 
     def source(self):
-        super().source()
-        return 
+       git = Git(self)
+       sources = self.conan_data["sources"][self.name]
+       # shallow checkout
+       git.fetch_commit(url=sources["url"], commit=f"v{self.version}")
+    
+    def build_requirements(self):
+        self.tool_requires(self.conan_data["tool_reqs"]["cmake"])
+        self.build_requires(self.conan_data["tool_reqs"]["python3.10"])
 
     def requirements(self):
-        self.requires(f"fep_sdk_participant/3.1.0@{self.user}/{self.channel}")
-        self.requires("boost/1.75.0")
+        self.requires(f"fep_sdk_participant/3.2.0@{self.user}/{self.channel}", private=True)
+        self.requires(self.conan_data["tool_reqs"]["pybind11"], private=True)
 
     def build(self):
-        patch_str = (Path(self.source_folder) / "patch1.diff").read_text()
+        patch_str = (Path(self.source_folder) / "../patch.diff").read_text()
         patch(self, patch_string=patch_str)
-
         cmake = CMake(self)
         cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
         cmake.configure(defs={"fep3_system_cmake_enable_documentation": "OFF", 
@@ -47,6 +53,9 @@ class ConanProduct(ConanFile):
                               "fep3_system_cmake_enable_functional_tests": "OFF",
                               "fep3_system_cmake_enable_private_tests": "OFF",
                               "Boost_USE_STATIC_LIBS": "On",
+                              "fep3_participant_use_rtidds": "Off",
+                              "fep3_system_cmake_python_version": "3.10",
+                            #   "pybind11_ROOT": pybind11_path
                              }
                         )
         cmake.build()
